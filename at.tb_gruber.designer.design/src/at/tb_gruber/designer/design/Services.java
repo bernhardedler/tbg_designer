a4348ce5b33c794ca52c870e292e47ce35346189
Bernhard Edler <bernhard.edler@gmail.com>
2020-02-07 14:20:43 +0100

    Verschiedene Größen Versorgungsknoten eingebaut

--- at.tb_gruber.designer.design/src/at/tb_gruber/designer/design/Services.java
+++ at.tb_gruber.designer.design/src/at/tb_gruber/designer/design/Services.java
@@ -65,11 +65,11 @@ public class Services {
 		}
 		return false;
 	}
-	
+
 	public Boolean isAnlageart(EObject self, Integer anlageart, Integer spannungsart) {
 		return isAnlageart(self, anlageart) && isSpannungsart(self, spannungsart);
 	}
-	
+
 	private Boolean isTrafoSpannungsart(EObject self, Integer target) {
 		if (self instanceof AnlageImpl) {
 			return target.equals(((AnlageImpl) self).getTrafospannung().getValue());
@@ -78,7 +78,18 @@ public class Services {
 	}
 
 	public Boolean isTrafoart(EObject self, Integer spannungsart, Integer trafoSpannungsart) {
-		boolean result = isAnlageart(self, anlagearttype.TRAFO_VALUE) && isSpannungsart(self, spannungsart) && isTrafoSpannungsart(self, trafoSpannungsart);
-		return result;
+		return isAnlageart(self, anlagearttype.TRAFO_VALUE) && isSpannungsart(self, spannungsart)
+				&& isTrafoSpannungsart(self, trafoSpannungsart);
+	}
+
+	public Boolean isVersorgungsknotenSize(EObject self, Integer spannungsart, String size) {
+		return isAnlageart(self, anlagearttype.VERSORGUNGSKNOTEN_VALUE)
+				&& isSpannungsart(self, spannungsart)
+				&& (size.equals(getVersorgungsknotenSize(self)));
+	}
+
+	public String getVersorgungsknotenSize(EObject self) {
+		int cnt = ((AnlageImpl) self).getVerbindungNach().size();
+		return (cnt >= 0 && cnt <= 6) ? "s" : (cnt >= 7 && cnt <= 12) ? "m" : "l";
 	}
 }
