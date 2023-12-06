4ca3df7472aae86f514150df18ae53787335b163
Bernhard Edler <bernhard.edler@gmail.com>
2023-12-06 19:23:52 +0100

    #32 Bei Öffnen von Datei und Sprung zum Editor ResetOrigin

--- at.tb_gruber.designer.design/src/at/tb_gruber/designer/design/Activator.java
+++ at.tb_gruber.designer.design/src/at/tb_gruber/designer/design/Activator.java
@@ -1,20 +1,26 @@
 package at.tb_gruber.designer.design;
 
-import java.io.FileInputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashSet;
-import java.util.Properties;
 import java.util.Set;
 
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
 import org.eclipse.sirius.diagram.ui.edit.internal.part.DiagramElementEditPartOperation;
+import org.eclipse.sirius.diagram.ui.tools.internal.edit.command.CommandFactory;
+import org.eclipse.sirius.diagram.ui.tools.internal.editor.DDiagramEditorImpl;
 import org.eclipse.sirius.viewpoint.description.Viewpoint;
+import org.eclipse.ui.IPartListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
+import at.tb_gruber.designer.gmf.editpolicy.TBGResetOriginModelChangeOperation;
 import at.tb_gruber.designer.ide.preferences.TBGPreferencePage;
 
 /**
@@ -23,7 +29,7 @@ import at.tb_gruber.designer.ide.preferences.TBGPreferencePage;
 public class Activator extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "at.tb_gruber.designer.design";
-	
+
 	// The shared instance
 	private static Activator plugin;
 
@@ -47,23 +53,61 @@ public class Activator extends AbstractUIPlugin {
 		viewpoints = new HashSet<Viewpoint>();
 		viewpoints.addAll(
 				ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/designer.odesign"));
-		
+
 		String dashLength = System.getProperty(TBGPreferencePage.DASH_LENGTH);
 		try {
 			float length = Float.parseFloat(dashLength);
-			
+
 			Field field = DiagramElementEditPartOperation.class.getDeclaredField("DASH_STYLE");
 			field.setAccessible(true);
-			
+
 			Field modifiersField = Field.class.getDeclaredField("modifiers");
 			modifiersField.setAccessible(true);
 			modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-			
+
 			field.set(null, new float[] { length, length });
 		} catch (NumberFormatException e) {
 			// do nothing
 		}
-			
+
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		window.getPartService().addPartListener(new IPartListener() {
+
+			@Override
+			public void partActivated(IWorkbenchPart part) {
+			}
+
+			@Override
+			public void partBroughtToTop(IWorkbenchPart part) {
+				resetOriginIfSiriusEditor(part);
+			}
+
+			@Override
+			public void partClosed(IWorkbenchPart part) {
+			}
+
+			@Override
+			public void partDeactivated(IWorkbenchPart part) {
+			}
+
+			@Override
+			public void partOpened(IWorkbenchPart part) {
+				resetOriginIfSiriusEditor(part);
+			}
+
+			private void resetOriginIfSiriusEditor(IWorkbenchPart part) {
+				if (part instanceof DDiagramEditorImpl && "Versorgungsschema".equals(part.getTitle())) {
+					DDiagramEditorImpl impl = (DDiagramEditorImpl) part;
+					TBGResetOriginModelChangeOperation operation = new TBGResetOriginModelChangeOperation(
+							(DiagramEditPart) impl.getDiagramEditPart());
+					DiagramEditPart editPart = impl.getDiagramEditPart();
+					RecordingCommand command = CommandFactory.createRecordingCommand(((IGraphicalEditPart) editPart).getEditingDomain(),
+							operation);
+					impl.getEditingDomain().getCommandStack().execute(command);
+				}
+			}
+		});
+
 	}
 
 	/*
